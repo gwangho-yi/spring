5c6db86743d44995214c7de8858b9338b27ca98e
gwangho-y <yigwangho21@gmail.com>
2023-08-16 17:59:40 +0900

    멤버 구현

---
+++ 스프링핵심원리/객체지향설계와 스프링/core/src/main/java/com/example/core/MemberApp.java
@@ -0,0 +1,17 @@
+package com.example.core;
+
+import com.example.core.member.Grade;
+import com.example.core.member.Member;
+import com.example.core.member.MemberService;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findByMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
