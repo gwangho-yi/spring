ad68c5ec11375e8619a70fb7a951e22f092b13c1
gwangho-y <yigwangho21@gmail.com>
2024-05-27 22:19:03 +0900

    탐색 위치와 기본 스캔 대상

--- 스프링핵심원리/객체지향설계와 스프링/core/src/main/java/com/example/core/AutoAppConfig.java
+++ 스프링핵심원리/객체지향설계와 스프링/core/src/main/java/com/example/core/AutoAppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan (
-
+        basePackages = "com.example.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
