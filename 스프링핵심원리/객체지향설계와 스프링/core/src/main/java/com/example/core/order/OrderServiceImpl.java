e9494deb9ae992987299d3d2c4e8ff6c5372d7dc
gwangho-y <yigwangho21@gmail.com>
2024-06-03 22:29:50 +0900

    @Quilifier, @Primary

--- 스프링핵심원리/객체지향설계와 스프링/core/src/main/java/com/example/core/order/OrderServiceImpl.java
+++ 스프링핵심원리/객체지향설계와 스프링/core/src/main/java/com/example/core/order/OrderServiceImpl.java
@@ -1,22 +1,27 @@
 package com.example.core.order;
 
+import com.example.core.annotation.MainDiscountPolicy;
 import com.example.core.discount.DiscountPolicy;
-import com.example.core.discount.FixDiscountPolicy;
-import com.example.core.discount.RateDiscountPolicy;
 import com.example.core.member.Member;
 import com.example.core.member.MemberRepository;
-import com.example.core.member.MemoryMemberRepository;
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
-public class OrderServiceImpl implements OrderService{
+public class OrderServiceImpl implements OrderService {
 
+    @Getter
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy rateDiscountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = rateDiscountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -26,8 +31,4 @@ public class OrderServiceImpl implements OrderService{
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
-    // 테스트용
-    public MemberRepository getMemberRepository() {
-        return memberRepository;
-    }
 }
