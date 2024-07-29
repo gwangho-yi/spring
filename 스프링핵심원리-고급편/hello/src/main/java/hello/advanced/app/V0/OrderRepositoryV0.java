5a889deb236e095bfc1e23b442b6686f4cb49651
gwangho-y <yigwangho21@gmail.com>
2024-07-29 21:13:36 +0900

    예외 프로젝트

---
+++ 스프링핵심원리-고급편/hello/src/main/java/hello/advanced/app/V0/OrderRepositoryV0.java
@@ -0,0 +1,36 @@
+package hello.advanced.app.V0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * packageName    : com.kovo.domain.ticketlink
+ * fileName       : OrderRepositoryV0
+ * author         : 이광호
+ * date           : 2024-07-29
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-07-29        이광호       최초 생성
+ */
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생!");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ 스프링핵심원리-고급편/hello/src/main/java/hello/advanced/app/V0/OrderServiceV0.java
@@ -0,0 +1,25 @@
+package hello.advanced.app.V0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * packageName    : com.kovo.domain.ticketlink
+ * fileName       : OrderService
+ * author         : 이광호
+ * date           : 2024-07-29
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-07-29        이광호       최초 생성
+ */
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV0 {
+    private final OrderRepositoryV0 orderRepository;
+
+    public void orderItem(String itemId) {
+        orderRepository.save(itemId);
+    }
+}
