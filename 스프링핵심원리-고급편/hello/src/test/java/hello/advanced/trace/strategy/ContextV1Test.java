3e9122e965e21c105618c1de8f9ca9143bba14e8
gwangho-y <yigwangho21@gmail.com>
2024-08-15 15:58:15 +0900

    전략 패턴 조립 실행 동시 적용

--- 스프링핵심원리-고급편/hello/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ 스프링핵심원리-고급편/hello/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -1,6 +1,7 @@
 package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import hello.advanced.trace.teamplate.code.AbstractTemplate;
@@ -24,7 +25,7 @@ import org.junit.jupiter.api.Test;
 @Slf4j
 public class ContextV1Test {
     @Test
-    void strategyV1 () {
+    void strategyV0 () {
         logic1();
         logic2();
     }
@@ -55,7 +56,7 @@ public class ContextV1Test {
      *  */
 
     @Test
-    void strategy() {
+    void strategyV1() {
         StrategyLogic1 strategyLogic1 = new StrategyLogic1();
         ContextV1 context1 = new ContextV1(strategyLogic1);
         context1.execute();
@@ -64,5 +65,69 @@ public class ContextV1Test {
         ContextV1 context2 = new ContextV1(strategyLogic2);
         context2.execute();
     }
+
+    @Test
+    void strategyV2() {
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        log.info("strategyLogic1={}", strategyLogic1.getClass());
+        contextV1.execute();
+
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        log.info("strategyLogic2={}", strategyLogic2.getClass());
+        contextV2.execute();
+
+
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV1 contextV1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+
+        contextV1.execute();
+
+
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+
+        contextV2.execute();
+
+
+    }
+
+    @Test
+    void strategyV4() {
+
+        ContextV1 contextV1 = new ContextV1(()-> log.info("비즈니스 로직1 실행"));
+        contextV1.execute();
+
+
+        ContextV1 contextV2 = new ContextV1(()-> log.info("비즈니스 로직2 실행"));
+        contextV2.execute();
+        contextV2.execute2();
+
+    }
+
 
 }
--- 스프링핵심원리-고급편/hello/src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
+++ 스프링핵심원리-고급편/hello/src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
@@ -7,32 +7,16 @@ import lombok.extern.slf4j.Slf4j;
  * fileName       : ContextV1
  * author         : 이광호
  * date           : 2024-08-15
- * description    :
+ * description    : 전략을 파라메터로 전달 받는 형식
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
  * 2024-08-15        이광호       최초 생성
  */
 @Slf4j
-public class ContextV1 {
+public class ContextV2 {
 
-    private Strategy strategy;
-
-    public ContextV1(Strategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public void execute() {
-        long startTime = System.currentTimeMillis();
-        // 비즈니스 로직 실행
-        strategy.call(); // 위임
-        // 비즈니스 로직 종료
-        long endTime = System.currentTimeMillis();
-        long resultTime = endTime - startTime;
-        log.info("resultTime={}", resultTime);
-    }
-
-    public void execute2() {
+    public void execute( Strategy strategy) {
         long startTime = System.currentTimeMillis();
         // 비즈니스 로직 실행
         strategy.call(); // 위임
