1f492075a255698c1525bac6a63a8a8de343d592
gwangho-y <yigwangho21@gmail.com>
2024-09-01 22:02:54 +0900

    액츄에이터, 마이크로미터

---
+++ 스프링부트 핵심원리와 활용/actuator/src/main/java/hello/controller/LogController.java
@@ -0,0 +1,32 @@
+package hello.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : com.kovo.domain.ticketlink
+ * fileName       : LogController
+ * author         : 이광호
+ * date           : 2024-09-01
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-09-01        이광호       최초 생성
+ */
+@Slf4j
+@RestController
+public class LogController {
+
+    @GetMapping("/log")
+    public String log() {
+        log.trace("trace log");
+        log.debug("debug log");
+        log.info("info log");
+        log.warn("warn log");
+        log.error("error log");
+        return "ok";
+    }
+}
