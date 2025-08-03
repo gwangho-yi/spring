aa33e0820a90134844ba570633a7ccfba48fa495
gwangho-yi <dlrhkdgh21@gmail.com>
2025-08-03 18:49:41 +0900

    AOP 패키지 import 시 자동 빈후처리기 등록

--- 스프링핵심원리-고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ 스프링핵심원리-고급편/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,6 +1,7 @@
 package hello.proxy;
 
 import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -18,7 +19,8 @@ import org.springframework.context.annotation.Import;
 // @Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
 //@Import(ProxyFactoryConfigV1.class)
-@Import(BeanPostProcessorConfig.class)
+//@Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의 AppV1Config 은 컴포넌트 스캔 대상에서 제외하고 설정을 갈아 끼우기 위해서 스캔 대상을 지정함
 public class ProxyApplication {
 
